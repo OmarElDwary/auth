d4a8689538e1f9b144ea194f0ae65bcf1e0e5467
Omar ElDwary <omareldwary91@gmail.com>
2023-07-04 19:41:36 +0300

    middleware and user route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,50 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import {toast} from "react-hot-toast";
+import {useRouter} from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function ProfilePage() {
+
+    const router = useRouter();
+    const [data, setData] = useState("nothing");
+
+    const logout = async () => {
+        try {
+            const response = await axios.get("/api/users/logout");
+            console.log(response);
+            toast.success("Logout successful");
+            router.push("/login");
+        } catch (error: any) {
+            console.log(error);
+            toast.error(error.response.data);
+        }
+    }
+
+    // const getProfile = async () => {
+    //     const response = await axios.get("/api/users/user");
+    //     console.log(response);
+    //     setData(response.data.data._id);
+    // }
+
+    useEffect(() => {
+        const getProfile = async () => {
+            const response = await axios.get("/api/users/user");
+            console.log(response);
+            setData(response.data.data.username);
+        }
+        getProfile();
+    }, [])
+    
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
-            <p>Username:</p>
+            <p>{data}</p>
+            <hr />
+            <button onClick={logout} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white rounded py-2 px-4">Logout</button>
+            {/* <button onClick={getProfile} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white rounded py-2 px-4">get user details</button> */}
         </div>
     )
 }
---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    const path = req.nextUrl.pathname;
+
+    const isPublic = path === "/login" || path === "/signup";
+
+    const token = req.cookies.get("token")?.value || '';
+    if (isPublic && token) {
+        return NextResponse.redirect(new URL("/profile", req.nextUrl));
+    }
+
+    if (!isPublic && !token) {
+        return NextResponse.redirect(new URL("/login", req.nextUrl));
+    }
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/profile',
+        '/login',
+        '/signup',
+    ]
+}
